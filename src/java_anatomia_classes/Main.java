5ae07991cc3004719bdfc1c64583cd19724725b5
alexs <alexs@DESKTOP-EIBDV7K>
2026-05-25 21:05:51 -0300

    Primeiro Commit

---
+++ src/java_anatomia_classes/Main.java
@@ -0,0 +1,20 @@
+package java_anatomia_classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String primeiroNome = "Alexsander";
+		String segundoNome = "Benicio";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		
+		System.out.println(nomeCompleto);
+		
+	}
+	
+	public static String nomeCompleto (String primeiroNome, String segundoNome) {
+		return primeiroNome.concat(" ").concat(segundoNome);
+	}
+
+}
